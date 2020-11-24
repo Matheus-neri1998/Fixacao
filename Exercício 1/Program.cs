be0c0e951094f43cc031bed3713472af714381bb
Matheus <mthneri@gmail.com>
2020-11-24 10:13:22 -0300

    Ex. 1

---
+++ Exercício 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercício_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ex. 1) Ler o ano atual e o ano de nascimento de uma pessoa. Escrever uma mensagem que diga se
+            ela poderá ou não votar este ano (não é necessário considerar o mês em que a pessoa nasceu) */
+
+            string [] anoatual = new string[1];
+            string [] anodenascimento = new string [1];
+
+            for (int i = 0; i < anoatual.Length; i++)
+            {
+                Console.WriteLine("Qual o ano atual? ");
+                anoatual[i] = Console.ReadLine();
+
+                for (int a = 0; a < anodenascimento.Length; a++)
+                {
+                    Console.WriteLine("Qual o seu ano de nascimento? ");
+                    anodenascimento [i] = Console.ReadLine();
+                }
+
+                 
+            }
+              
+
+        }
+    }
+}
