9e8206e96fed78c708dbcc0f15ca60c933b5eeb6
Matheus <mthneri@gmail.com>
2020-11-25 16:06:38 -0300

    Exercício 5 feito

---
+++ Exercício 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercício_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Tabuada;
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Veja a tabuada: ");
+            // Console.WriteLine("-------------");
+            Console.ResetColor();
+
+            for (var i = 1; i <= 10; i++)
+            {
+            Console.WriteLine("-------------");
+                for (var j = 1; j <= 10; j++)
+                {
+                    Tabuada = i * j;
+                    Console.WriteLine($"{i} * {j} + {Tabuada}");
+
+                }                
+            }
+        }
+    }
+}
